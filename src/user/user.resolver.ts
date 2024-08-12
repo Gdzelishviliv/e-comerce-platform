d956917049479b35a472b22a0ebac543bc28760f
Ivane <gdzelishviliv@gmail.com>
2024-08-12 17:21:55 +0400

    refactor user resolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,21 +1,26 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { UserDto, CreateUserInput, UpdateUserInput } from './dto/user.dto';
+import { User } from './schemas/user.schema';
 
 @Resolver(() => UserDto)
 export class UserResolver {
   constructor(private userService: UserService) { }
 
-  @Mutation(() => UserDto)
-  async createUser(@Args('input') input: CreateUserInput): Promise<UserDto> {
-    const user = await this.userService.create(input);
+  private mapToUserDto(user: User): UserDto {
     return {
       id: user._id,
       username: user.username,
       email: user.email,
       avatar: user.avatar,
-      isActive: user.isActive,
-    };
+      isActive: user.isActive
+    }
+  }
+
+  @Mutation(() => UserDto)
+  async createUser(@Args('input') input: CreateUserInput): Promise<UserDto> {
+    const user = await this.userService.create(input);
+    return this.mapToUserDto(user);
   }
 
   @Mutation(() => UserDto)
@@ -24,13 +29,7 @@ export class UserResolver {
     @Args('input') input: UpdateUserInput,
   ): Promise<UserDto> {
     const user = await this.userService.update(id, input);
-    return {
-      id: user._id,
-      username: user.username,
-      email: user.email,
-      avatar: user.avatar,
-      isActive: user.isActive,
-    };
+    return this.mapToUserDto(user);
   }
 
   @Mutation(() => Boolean)
@@ -42,36 +41,18 @@ export class UserResolver {
   @Mutation(() => UserDto)
   async deactivateUser(@Args('id') id: string): Promise<UserDto> {
     const user = await this.userService.deactivate(id);
-    return {
-      id: user._id,
-      username: user.username,
-      email: user.email,
-      avatar: user.avatar,
-      isActive: user.isActive,
-    };
+    return this.mapToUserDto(user);
   }
 
   @Mutation(() => UserDto)
   async activateUser(@Args('id') id: string): Promise<UserDto> {
     const user = await this.userService.activate(id);
-    return {
-      id: user._id,
-      username: user.username,
-      email: user.email,
-      avatar: user.avatar,
-      isActive: user.isActive,
-    };
+    return this.mapToUserDto(user);
   }
 
   @Query(() => UserDto)
   async user(@Args('id') id: string): Promise<UserDto> {
     const user = await this.userService.findById(id);
-    return {
-      id: user._id,
-      username: user.username,
-      email: user.email,
-      avatar: user.avatar,
-      isActive: user.isActive,
-    };
+    return this.mapToUserDto(user);
   }
 }
